429de2eda5f693ab8bd6d785efcf5ae0077021ba
ipcrm <mcadorette@gmail.com>
2019-10-18 15:18:14 -0400

    interface update

--- lib/sdm/serverless/deployer.ts
+++ lib/sdm/serverless/deployer.ts
@@ -20,7 +20,10 @@ export const configuration = configure(async sdm => {
     const dev = new ServerlessDeploy()
         .with({
             deployArgs: { stage: "dev" },
-            registrationName: "serverless-deployer",
+            remoteExecution: {
+                registrationName: "serverless-deployer",
+                stage: "dev",
+            },
         });
 }, {
     postProcessors: [
